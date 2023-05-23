f53cbc6fbc593973b628f092ee67413fc0fc0fc0
simple123w <122342661+Asliddin18@users.noreply.github.com>
2023-05-23 13:52:33 +0500

    12
    12

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,30 +1,4 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\app\\page.js": [
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Navbar.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Home.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Featured.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Popular.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\node_modules\\swiper\\swiper.min.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\node_modules\\swiper\\modules\\navigation\\navigation.min.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Footer.css"
-    ],
-    "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\app\\layout.js": [
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\app\\globals.css"
-    ]
-  },
-  "cssModules": {
-    "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\app\\page": [
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Home.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Featured.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Footer.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\app\\globals.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Navbar.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\src\\pages\\css\\Popular.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\node_modules\\swiper\\swiper.min.css",
-      "C:\\Users\\Alfatech.uz\\Documents\\GitHubTiny\\avto\\node_modules\\swiper\\modules\\navigation\\navigation.min.css"
-    ]
-  }
+  "cssImports": {},
+  "cssModules": {}
 }
